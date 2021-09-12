d3e405e5784b6e21590db54641ea868a74217db6
Andrey <andreyevyk13@gmail.com>
2021-09-11 22:11:30 -0300

    finish challange

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      done: false,
+      title: newTaskTitle,
+    }
+    setTasks(oldTasks => [...oldTasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => {
+      if(task.id === id){
+        return {
+          ...task,
+          done: !task.done
+        }
+      }
+      return task;
+    })
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
